f64cafb8e543a1aeabfc124e43e11cff38be86a2
Kevin Corbin <kecorbin@cisco.com>
2014-04-04 07:21:13 -0500

    updated for cobra sdk

--- csvfilters.py
+++ csvfilters.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
+'''
+create tenant filters based on input csv file
+updated for build 872
+'''
 
 import sys
 import csv
 from argparse import ArgumentParser
-sys.path.append('pysdk')
-from insieme.mit import access
-
-
+#sys.path.append('~/cobra')
+import cobra.mit.access
+import cobra.mit.session
+import cobra.mit.request
+import cobra.model.fv
+import cobra.model.vz
 parser = ArgumentParser(sys.argv[0])
 parser.add_argument('-a', '--apic', help='IP Address of APIC', required=True)
 parser.add_argument('-u', '--user', help='Username for APIC', required=True)
@@ -20,26 +26,31 @@ username = args.user
 password = args.password
 filename = args.filename
 tenant = args.tenant
-access.rest()
-directory = access.MoDirectory(ip=hostname, port='8000', user=username, password=password)
+#cobra.mit.access.access.rest()
+
+ep = cobra.mit.access.EndPoint(hostname, secure=False, port=80)
+ls = cobra.mit.session.LoginSession(username,password)
+directory = cobra.mit.access.MoDirectory(ep,ls)
+directory.login()
 
 def main(directory,filename,tenant):
-   directory = access.MoDirectory(ip=hostname, port='8000', user=username, password=password)
    polUni = directory.lookupByDn('uni')
-   fvTenant = directory.create('fv.Tenant', polUni, name=tenant)
+   fvTenant = cobra.model.fv.Tenant(polUni, name=tenant)
    
    with open(filename, 'rb') as infile:
 
         filter_list = csv.reader(infile, delimiter=',',quotechar='|')
         for row in filter_list:
-            vzFilter = directory.create('vz.Filter', fvTenant, name=row[0])
-            vzEntry = directory.create('vz.Entry', vzFilter, etherT=row[1], prot=row[2], dFromPort=row[3], dToPort=row[3], name=row[0]+'-entry')
+            vzFilter = cobra.model.vz.Filter(fvTenant, name=row[0])
+            vzEntry = cobra.model.vz.Entry(vzFilter, etherT=row[1], prot=row[2], dFromPort=row[3], dToPort=row[3], name=row[0]+'-entry')
 
 
-	    vzBrCP = directory.create('vz.BrCP', fvTenant, name=row[0]+'-contract')
-            vzSubj = directory.create('vz.Subj', vzBrCP, name=row[0]+'-subject')
-            vzRsSubjFiltAtt = directory.create('vz.RsSubjFiltAtt', vzSubj, tnVzFilterName=row[0])
+	    vzBrCP = cobra.model.vz.BrCP(fvTenant, name=row[0]+'-contract')
+            vzSubj = cobra.model.vz.Subj(vzBrCP, name=row[0]+'-subject')
+            vzRsSubjFiltAtt = cobra.model.vz.RsSubjFiltAtt(vzSubj, tnVzFilterName=row[0])
 
-   sd = directory.commit(fvTenant)
+   c = cobra.mit.request.ConfigRequest()
+   c.addMo(polUni)	
+   sd = directory.commit(c)
 
 main(directory,filename,tenant)
